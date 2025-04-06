633d7664c2378e3e3aac7a6d522bad8a640b52c2
Wayne113 <wayne_113@outlook.com>
2025-04-07 02:33:19 +0800

    1

--- Demo/ContentView.swift
+++ Demo/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("好难啊!")
+
+        ZStack {
+            
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                
+                Spacer()
+                Image("button")
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player").font(.headline).foregroundColor(Color.white)
+                        Text("0").font(.largeTitle).foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU").font(.headline)
+                            .foregroundColor(Color.white)
+                        Text("0").font(.largeTitle).foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
+        
     }
 }
 
